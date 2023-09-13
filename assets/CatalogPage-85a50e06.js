e0df2e38789511e23703266bef5d3db6c93c08ec
Nataliya Kim <magistr1982@gmail.com>
2023-09-13 13:36:09 +0300

    Updates

---
+++ assets/CatalogPage-85a50e06.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-a61a65d3.js";const e=()=>a.jsx("div",{children:a.jsx("h1",{children:"CatalogPage"})});export{e as default};
---
+++ assets/HomePage-1e605f31.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-a61a65d3.js";const o=()=>e.jsx("div",{children:e.jsx("h1",{children:"HomePage"})});export{o as default};

b8d13f732f684eb88654273d1c95db37a9c56ea7
Nataliya Kim <magistr1982@gmail.com>
2023-09-14 17:04:23 +0300

    Updates

--- assets/CatalogPage-0f2c388e.js
+++
@@ -1,9 +0,0 @@
-import{n,j as t}from"./index-3ab68216.js";const i=n.section`
-  width: 100%;
-  min-height: 100vh;
-  padding: 150px 120px;
-`,o=n.div`
-  width: 100%;
-  min-height: 100vh;
-  background-color: gray;
-`,a=()=>t.jsx(i,{children:t.jsx(o,{})});export{a as default};
--- assets/FavoritePage-5d162b37.js
+++ assets/FavoritePage-5d162b37.js
@@ -1,4 +1,4 @@
-import{n,j as t}from"./index-3ab68216.js";const i=n.section`
+import{n,j as t}from"./index-400959c1.js";const i=n.section`
   width: 100%;
   min-height: 100vh;
   padding: 150px 120px;
